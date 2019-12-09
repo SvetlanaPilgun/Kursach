4d19077d6eeec061d70b7cd72afa813fac3e119c
SvetlanaPilgun <55964156+SvetlanaPilgun@users.noreply.github.com>
2019-12-09 13:27:16 +0200

    добавление коментариев и отладка программы

--- Main_kurs.py
+++ Main_kurs.py
@@ -2,12 +2,37 @@ import People_kurs
 
 
 list_all_chuvaki = People_kurs.Kontroler('C:\\Users\\svetlana.pilgun\\PycharmProjects\\Projects_test\\File_kurs.txt').readFile()
-""" пытяюсь посомтрть что считывается """
+""" считываем данные из файла"""
 
 People_kurs.Kontroler.DisplayData(list_all_chuvaki)
-""" пытяюсь посомтрть что выводится """
+""" выводим содержимое файла """
 student_list, prepod_List = People_kurs.Kontroler.person_list(list_all_chuvaki)
-print ('Студенты: ')
-People_kurs.Kontroler.printList(student_list)
+""" создаем два отдельных списка студентов и преподавателей """
+print('Студенты: ')
+print()
+People_kurs.Kontroler.printList1(student_list)
+""" выводим студентов """
 People_kurs.Kontroler.sort_list(student_list)
+""" сортируем студентов """
+print('------------------------------------------------------------------')
+print('Отсортированные студенты по имени: ')
+print()
+People_kurs.Kontroler.printList1(student_list)
+""" выводим уже отсортированных студентов """
+print('------------------------------------------------------------------')
+print('Преподаватели: ')
+print()
+People_kurs.Kontroler.printList2(prepod_List)
+""" выводим преподавателей """
+People_kurs.Kontroler.sort_list(prepod_List)
+""" сортируем преподавателей """
+print('------------------------------------------------------------------')
+print('Отсортированные преподаватели по имени: ')
+print()
+People_kurs.Kontroler.printList2(prepod_List)
+""" выводим уже отсортированных преподавателей """
+
+
+
+
 
--- People_kurs.py
+++ People_kurs.py
@@ -1,16 +1,7 @@
-'''class Univer_People:
-    def __init__(self, vtype, name, surname, birthday, facultet):
-        self.vtype = vtype
-        self.name = name
-        self.surname = surname
-        self.birthday = birthday
-        self.facultet = facultet
-        #self.money = money
-        #self.years = years
-'''
+
 class Student:
+    """ Класс студентов """
     def __init__(self, name, surname, birthday, stipend, facultet, kusrs):
-        #self.vtype = vtype
         self.name = name
         self.surname = surname
         self.birthday = birthday
@@ -18,9 +9,10 @@ class Student:
         self.stipend = stipend
         self.kusrs = kusrs
 
+
 class Prepod:
+    """ Класс преподавателей """
     def __init__(self, name, surname, birthday, zp, facultet, stazh):
-        #self.vtype = vtype
         self.name = name
         self.surname = surname
         self.birthday = birthday
@@ -28,7 +20,9 @@ class Prepod:
         self.zp = zp
         self.stazh = stazh
 
+
 class Kontroler:
+    """ Класс контролер """
     def __init__(self, path):
         self.path = path
 
@@ -55,77 +49,47 @@ class Kontroler:
         prepods = []
         for i in list:
             if i[0] == 'студент':
-                line = Student(i[1], i[2], i[3], i[4], i[5], i[6])
+                line = Student(i[name], i[surname], i[birthday], i[money], i[facultet], i[count_year])
                 students.append(line)
             elif i[0] == 'преподаватель':
-                line = Prepod(i[1], i[2], i[3], i[4], i[5], i[6])
+                line = Prepod(i[name], i[surname], i[birthday], i[money], i[facultet], i[count_year])
                 prepods.append(line)
         return students, prepods
 
     def DisplayData(list):
         """ Метод вывода информации """
+        type_p = 0
+        name = 1
+        surname = 2
+        birthday = 3
+        money = 4
+        facultet = 5
+        count_year = 6
         print('Список всех чуваков:')
         print()
-        print('%-14s' ': ' '%-10s' '%-12s' '%-17s' '%-14s' '%-11s' '%-5s' % ('Должность', 'Имя', 'Фамилия', 'Дата Рождения', 'стипендия/зп', 'Факульткет', 'Курс/Стаж'))
+        print('%-14s' ': ' '%-10s' '%-12s' '%-17s' '%-14s' '%-11s' '%-5s' % ('Должность', 'Имя', 'Фамилия', 'Дата_Рождения', 'стипендия/зп', 'Факульткет', 'Курс/Стаж'))
         print()
         for i in list:
-            print('%-14s' ': ' '%-10s' '%-12s' '%-17s' '%-14s' '%-11s' '%-5s' % (i[0], i[1], i[2], i[3], i[4], i[5], i[6]))
-        print('-----------------------------------------------------------------------------------')
+            print('%-14s' ': ' '%-10s' '%-12s' '%-17s' '%-14s' '%-11s' '%-5s' % (i[type_p], i[name], i[surname], i[birthday], i[money], i[facultet], i[count_year]))
+        print('---------------------------------------------------------------------------------------')
 
-    def older(arg1, arg2):
-        if int(arg1[2]) < int(arg2[2]):
-            return True
-        elif int(arg1[2]) == int(arg2[2]) and int(arg1[1]) < int(arg2[1]):
-            return True
-        elif int(arg1[2]) == int(arg2[2]) and int(arg1[1]) == int(arg2[1]) and int(arg1[0]) < int(arg2[0]):
-            return True
-        else:
-            return False
-
-    def sort_list(list):
-        #iOlderUser = list[j]
-        #print(iOlderUser)
-        for i in list:
-            iOlderUser = list[0]
-            date = i.birthday.split('.')
-            dateOlder = iOlderUser.birthday.split('.')
-            #print(date)
-            #print(dateOlder)
-            #print(i)
-            if Kontroler.older(date, dateOlder):
-                i, iOlderUser = iOlderUser, i
-
-            #else:
-               # list.sort(key=lambda i: i.name)
-                #print(list.i)
-                #print(list.iOlderUser)
-                #list.sort(key=lambda i: i.name)
-
-        return list
-
-
-
-
-
-    def printList(list):  # function for printing lists
+    def printList1(list):
+        """ Метод вывода студентов """
         for i in list:
             print('%-12s %-12s %-14s %-12s %-10s %-5s'
                   % (i.name, i.surname, i.birthday, i.stipend, i.facultet, i.kusrs))
 
+    def printList2(list):
+        """ Метод вывода преподавателей """
+        for i in list:
+            print('%-12s %-12s %-14s %-12s %-10s %-5s'
+                  % (i.name, i.surname, i.birthday, i.zp, i.facultet, i.stazh))
+
+
+    def sort_list(list):
+        """ Метод сортировки списка по имени """
+        list.sort(key=lambda i: i.name)
+        return list
 
 
 
-list_all_chuvaki = Kontroler('C:\\Users\\svetlana.pilgun\\PycharmProjects\\Projects_test\\File_kurs.txt').readFile()
-""" пытяюсь посомтрть что считывается """
-
-#print (list_all_chuvaki)
-
-#Kontroler.DisplayData(list_all_chuvaki)
-""" пытяюсь посомтрть что выводится """
-
-student_list, prepod_List = Kontroler.person_list(list_all_chuvaki)
-print ('Студенты: ')
-Kontroler.printList(student_list)
-Kontroler.sort_list(student_list)
-
-Kontroler.printList(student_list)
